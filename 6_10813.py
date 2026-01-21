c8bb98c6e77a2923280abe527b000aba85887825
sje <dltnwjd2304@gmail.com>
2026-01-22 02:07:46 +0900

    문제잘못읽은거수정

--- 6_10813.py
+++ 6_10813.py
@@ -1,10 +1,7 @@
 # 공바꾸기
 N, M = map(int, input().split())
-basket = [0]*(N+1)
+basket = list(range(N+1))
 for _ in range(M):
     i,j = map(int, input().split())
-    ii = i*1
-    jj = j*1
-    basket[i] = jj
-    basket[j] = ii
-print(*basket[1:])
+    basket[i],basket[j]=basket[j],basket[i]
+print(*basket)
