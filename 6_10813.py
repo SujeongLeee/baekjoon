81df46e0a386ad55bc53b9f5ba3e762d06834166
sje <dltnwjd2304@gmail.com>
2026-01-22 02:01:21 +0900

    add ii and jj

--- 6_10813.py
+++ 6_10813.py
@@ -3,6 +3,8 @@ N, M = map(int, input().split())
 basket = [0]*(N+1)
 for _ in range(M):
     i,j = map(int, input().split())
-    basket[i] = j
-    basket[j] = i
+    ii = i*1
+    jj = j*1
+    basket[i] = jj
+    basket[j] = ii
 print(*basket[1:])
