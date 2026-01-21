fb4ddeb8cfc28a33c58db5536241a7e9372abe19
sje <dltnwjd2304@gmail.com>
2026-01-22 03:14:03 +0900

    reverse를 gpt의 도움을 받아..

--- 9_10811.py
+++ 9_10811.py
@@ -3,5 +3,5 @@ n,m = map(int, input().split())
 baskets = list(range(1,n+1))
 for _ in range(m):
     i,j = map(int, input().split())
-    baskets[i:j] = baskets[j:i]
-print(baskets)
+    baskets[i-1:j] = baskets[i-1:j][::-1] # 이게 뒤집는 것 같음
+print(*baskets)
