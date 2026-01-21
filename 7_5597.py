c493dc51efe2004c200f0b5d4b9432d8c506ceb8
sje <dltnwjd2304@gmail.com>
2026-01-22 03:28:23 +0900

    move files to the stage folder

--- 7_5597.py
+++
@@ -1,9 +0,0 @@
-# 과제 안 내신 분..?
-numbers = list(range(1,31))
-for _ in range(28):
-    n = int(input())
-    if n in numbers:
-        numbers.remove(n) # numbers에서 n 과 똑같은거 찾아서 제거
-
-print(min(*numbers))
-print(max(*numbers))
