4b408ce54696b21681c10eb3b51bb711e53be562
sje <dltnwjd2304@gmail.com>
2026-01-22 03:09:55 +0900

    n을m으로변경

--- 9_10811.py
+++ 9_10811.py
@@ -1,7 +1,7 @@
 # 바구니 뒤집기
 n,m = map(int, input().split())
 baskets = list(range(1,n+1))
-for _ in range(n):
+for _ in range(m):
     i,j = map(int, input().split())
     baskets[i:j] = baskets[j:i]
 print(baskets)
