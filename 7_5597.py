98a9724bea3d56a17a5a12eed94d1b49ac512f06
sje <dltnwjd2304@gmail.com>
2026-01-22 02:22:06 +0900

    if 문 추가

--- 7_5597.py
+++ 7_5597.py
@@ -2,7 +2,8 @@
 numbers = list(range(1,31))
 for _ in range(29):
     n = int(input())
-    # numbers에서 n 과 똑같은거 찾아서 제거
+    if n in numbers:
+        numbers.remove(n) # numbers에서 n 과 똑같은거 찾아서 제거
 
 print(min(numbers))
 print(max(numbers))
