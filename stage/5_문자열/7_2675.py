42d6da4d6ead77e605fa28929a1a5ce2c6ea4c42
sje <dltnwjd2304@gmail.com>
2026-01-26 02:36:53 +0900

    correct

--- stage/5_문자열/7_2675.py
+++ stage/5_문자열/7_2675.py
@@ -1,10 +1,11 @@
 # sol1
 t = int(input())
 for _ in range(t):
-    rs = str(input())
-    r = int(rs[0])
-    s = rs[2:]
+    r,s = input().split()
+    r = int(r)
+    result = ""
     for i in range(len(s)):
-        print(s[i]*r, end="")
+        result += s[i]*r
+    print(result)
     
 
