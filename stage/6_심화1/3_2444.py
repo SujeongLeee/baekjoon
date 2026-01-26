8898c7c044cee60b4420042ac47fd13bdf425658
sje <dltnwjd2304@gmail.com>
2026-01-26 21:23:53 +0900

    range(n-1,0,-1)

--- stage/6_심화1/3_2444.py
+++ stage/6_심화1/3_2444.py
@@ -1,3 +1,9 @@
 n = int(input())
-for i in range(1,n+1):
-    print('*'*(2*i-1))
+for i in range(1,n+1): # 1~5
+    spaces = " "*(n-i)
+    stars = '*'*(2*i-1)
+    print(spaces+stars+spaces) # 4~1로 다시 줄어들어야 함
+for i in range(n-1,0,-1):
+    spaces = " "*(n-i)
+    stars = '*'*(2*i-1)
+    print(spaces+stars+spaces)
