696a87424c22f5fdf78d685b1576d47067ebae61
sje <dltnwjd2304@gmail.com>
2026-01-26 21:49:44 +0900

    print(1 if ... else 0)

--- stage/6_심화1/4_10988.py
+++ stage/6_심화1/4_10988.py
@@ -1,23 +1,29 @@
-# sol1
-s = input()
-slist = list(s)
-slen = len(slist)
-# print(slist); print(slen)
-if slen%2==0:
-    idx = int(slen/2)
-    left = slist[0:idx] # 여기는 0부터 끝까지 다 포함
-    right = slist[idx:slen][::-1] # 여기는 처음+1 부터 끝까지 포함 ?????
-    # print(left); print(right)
-    if left==right:
-        print(1)
-    else:
-        print(0)
-else:
-    idx = int((slen-1)/2)
-    left = slist[0:idx] # 여기는 0부터 끝까지 다 포함
-    right = slist[idx+1:slen][::-1] # 여기는 처음+1 부터 끝까지 포함 ?????
-    # print(left); print(right)
-    if left==right:
-        print(1)
-    else:
-        print(0)
+# # sol1
+# # 리스트[a:b] -> a 이상 b 미만
+# # 조잡....
+# s = input()
+# slist = list(s)
+# slen = len(slist)
+# # print(slist); print(slen)
+# if slen%2==0:
+#     idx = int(slen/2)
+#     left = slist[0:idx]
+#     right = slist[idx:slen][::-1]
+#     # print(left); print(right)
+#     if left==right:
+#         print(1)
+#     else:
+#         print(0)
+# else:
+#     idx = int((slen-1)/2)
+#     left = slist[0:idx]
+#     right = slist[idx+1:slen][::-1]
+#     # print(left); print(right)
+#     if left==right:
+#         print(1)
+#     else:
+#         print(0)
+
+# GPT
+s = input().strip()
+print(1 if s == s[::-1] else 0)
