619f6a03ead54f6e85c26690b2e3975e961f233e
sje <dltnwjd2304@gmail.com>
2026-01-28 02:14:14 +0900

    second

--- stage/7_2차원배열/4_2563.py
+++ stage/7_2차원배열/4_2563.py
@@ -1,25 +1,32 @@
 n = int(input())
-pair = []
+mat = [[0]*100 for _ in range(100)]
 
-for _ in range(n):
-    ldist, bdist = map(int, input().split())
-    pair.append([ldist, bdist])
 
-overlap = 0
-for i in range(n-1):
-    # print('i:',i)
-    for j in range(1,n-i):
-        # print('j:',j)
-        l = abs(pair[i][0]-pair[i+j][0])
-        r = abs(pair[i][1]-pair[i+j][1])
-        if l>= 10 or r >= 10: 
-            overlap += 0
-        else:
-            ll = abs(pair[i][0]+10 - pair[i+j][0])
-            rr = abs(pair[i+j][1]+10 - pair[i][1])
-            overlap += ll*rr
 
-# print(overlap)
-print((10**2)*3-overlap)
+# # 첫번째 시도
+# n = int(input())
+# pair = []
 
-# 어떤 영역이 여러번 겹치는 경우에는 ???????
+# for _ in range(n):
+#     ldist, bdist = map(int, input().split())
+#     pair.append([ldist, bdist])
+
+# overlap = 0
+# for i in range(n-1):
+#     for j in range(1,n-i):
+#         l = abs(pair[i][0]-pair[i+j][0])
+#         r = abs(pair[i][1]-pair[i+j][1])
+#         if l>= 10 or r >= 10: 
+#             overlap += 0
+#         else:
+#             ll = abs(pair[i][0]+10 - pair[i+j][0])
+#             rr = abs(pair[i+j][1]+10 - pair[i][1])
+#             overlap += ll*rr
+
+# # print(overlap)
+# print((10**2)*3-overlap)
+
+# # 어떤 영역이 여러번 겹치는 경우에는 ??????? 불가능 !!!
+
+
+
